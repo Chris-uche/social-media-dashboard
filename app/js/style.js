36b2290d8c90071a44e939661b409281ea330538
Chris-uche <Christogonus2010@yahoo.com>
2020-09-18 23:25:09 +0100

    The style js file

---
+++ app/js/style.js
@@ -0,0 +1,9 @@
+
+
+var checkbox = document.querySelector('#checkbox');
+var cards = document.querySelectorAll(".large-cards,.medium-cards");
+var body = document.querySelector('body')
+var before = document.querySelector('.before')
+var h = document.querySelector('h1,h2,h3,h4')
+var spanSubstitle = document.querySelector('span,.subtitle')
+
